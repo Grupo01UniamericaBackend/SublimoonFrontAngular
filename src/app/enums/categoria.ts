b643b616e8188495e8fc5b31f718d07f3522f040
Gabriel S. A <falconrei123@gmail.com>
2023-11-04 13:39:48 -0300

    criando enum e mudando model produto

---
+++ src/app/enums/categoria.ts
@@ -0,0 +1,6 @@
+export enum Categoria{
+    CANECA = 'CANECA',
+    GARRAFINHA = 'GARRAFINHA',
+    CAMISETA = 'CAMISETA',
+    COPOTERMICO = 'COPOTERMICO'
+}
--- src/app/models/produto.ts
+++ src/app/models/produto.ts
@@ -1,10 +1,12 @@
+import { Categoria } from "../enums/categoria";
+import { Cor } from "../enums/cor";
 import { AbstratcEntity } from "./abstratc-entity";
 
 export class Produto extends AbstratcEntity {
 
     nome!:string;
-    categoria!: string;
-    cor!: string;
+    categoria!: Categoria;
+    cor!: Cor;
     descricao!: string;
     imagem!: string;
     preco!: string;
